4d8b77f58d9bf78247a5d3787d41adf29fe84d59
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2023-12-19 19:00:27 +0530

    LinkedListsSort.java

--- Day_12/LinkedListsSort.java
+++ Day_12/LinkedListsSort.java
@@ -1,8 +1,20 @@
-class Solution
+/*
+Given two linked lists sorted in increasing order, create a new linked list representing the intersection of the two linked lists. The new linked list should be made with without changing the original lists.
+
+Note: The elements of the linked list are not necessarily distinct.
+
+Example 1:
+
+Input:
+LinkedList1 = 1->2->3->4->6
+LinkedList2 = 2->4->6->8
+Output: 2 4 6
+*/
+
+class LinkedListsSort
 {
    public static Node findIntersection(Node head1, Node head2)
     {
-        // code here.
          Node dummy = new Node(0); 
         Node intersectionList = dummy;
 
