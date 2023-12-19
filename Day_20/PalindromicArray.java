815c671d5ea810cd72bdfb6837ab134d4bd2d17a
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2023-12-19 19:47:53 +0530

    PalindromicArray.java

--- Day_20/PalindromicArray.java
+++ Day_20/PalindromicArray.java
@@ -1,4 +1,17 @@
-class GfG
+/*
+Given a Integer array A[] of n elements. Your task is to complete the function PalinArray. Which will return 1 if all the elements of the Array are palindrome otherwise it will return 0.
+
+Example 1:
+
+Input:
+5
+111 222 333 444 555
+
+Output:
+1
+*/
+
+class PalindromicArray
 {
 	public static int palinArray(int[] a, int n)
     {
