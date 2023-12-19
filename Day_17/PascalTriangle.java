379ee73c6869a26ae1bc5cc3aff00a00dc2983f4
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2023-12-19 19:40:43 +0530

    PascalTriangle.java

--- Day_17/PascalTriangle.java
+++ Day_17/PascalTriangle.java
@@ -1,6 +1,30 @@
-class Solution {
+/*
+Given a positive integer N, return the Nth row of pascal's triangle.
+Pascal's triangle is a triangular array of the binomial coefficients formed by summing up the elements of previous row.
+The elements can be large so return it modulo 109 + 7.
+
+File:PascalTriangleAnimated2.gif
+
+Example 1:
+
+Input:
+N = 4
+Output: 
+1 3 3 1
+Explanation: 
+4th row of pascal's triangle is 1 3 3 1.
+Example 2:
+
+Input:
+N = 5
+Output: 
+1 4 6 4 1
+Explanation: 
+5th row of pascal's triangle is 1 4 6 4 1.
+*/
+
+class PascalTriangle {
     ArrayList<Long> nthRowOfPascalTriangle(int n) {
-        // code here
          ArrayList<Long> pre = new ArrayList<>();
         long mod = 1000000007;
         for (int i = 0; i < n; i++) {
