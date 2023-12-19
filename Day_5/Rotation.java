c1c3dcaff26a85c777dd10cb6df920592de31d04
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2023-12-19 18:47:09 +0530

    Rotation.java

--- Day_5/Rotation.java
+++ Day_5/Rotation.java
@@ -1,7 +1,17 @@
-class Solution
+/*
+Given two strings a and b. The task is to find if the string 'b' can be obtained by rotating (in any direction) string 'a' by exactly 2 places.
+
+Example 1:
+
+Input:
+a = amazon
+b = azonam
+Output: 
+1
+*/
+
+class Rotation
 {
-    //Function to check if a string can be obtained by rotating
-    //another string by exactly 2 places.
     public static boolean isRotated(String str1, String str2)
     {
         if(str1.length()!=str2.length())
