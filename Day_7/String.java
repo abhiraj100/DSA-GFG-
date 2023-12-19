68ee7209b9da545db1b18453fe36b273d639dbec
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2023-12-19 18:50:34 +0530

    String.java

--- Day_7/String.java
+++ Day_7/String.java
@@ -1,19 +1,26 @@
+/*
+You are given two strings of equal lengths, s1 and s2. The task is to check if s2 is a rotated version of the string s1.
 
-class Solution
+Note: The characters in the strings are in lowercase.
+
+Example 1:
+
+Input:
+geeksforgeeks
+forgeeksgeeks
+Output: 
+1
+*/    
+class String
 {
-    //Function to check if two strings are rotations of each other or not.
     public static boolean areRotations(String s1, String s2 )
     {
-        // Your code here
-         // Check if lengths of both strings are the same
         if (s1.length() != s2.length()) {
             return false;
         }
 
-        // Concatenate s1 with itself
         String concatenated = s1 + s1;
 
-        // Check if s2 is a substring of the concatenated string
         return concatenated.contains(s2);
     }
 
