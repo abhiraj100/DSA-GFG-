318558ee6046d0a05ad0c2db7c0c8c2b449d790c
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2023-12-19 18:53:48 +0530

    PossibleString.java

--- Day_9/PossibleString.java
+++ Day_9/PossibleString.java
@@ -1,4 +1,15 @@
-class Solution{
+/*
+Given two integers N and K, the task is to find the string S of minimum length such that it contains all possible strings of size N as a substring. The characters of the string should be from integers ranging from 0 to K-1.  
+
+Example 1:
+
+Input:
+N = 2, K = 2
+Output: 
+00110
+*/    
+
+class PossibleString{
     public String findString(int n, int k){
             StringBuilder ans = new StringBuilder();
 
