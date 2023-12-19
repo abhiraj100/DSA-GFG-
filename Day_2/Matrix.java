2c1851269a8e395dd50d1789677dc26c5fa97745
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2023-12-19 18:40:04 +0530

    Matrix.java

--- Day_2/Matrix.java
+++ Day_2/Matrix.java
@@ -1,10 +1,21 @@
-class Solution
+/*
+Given a matrix(2D array) M of size N*N consisting of 0s and 1s only. The task is to find the column with maximum number of 0s. If more than one column exists, print the one which comes first. If the maximum number of 0s is 0 then return -1.
+
+Example:
+
+Input:
+N = 3
+M[][] = {{0, 0, 0},
+          {1, 0, 1},
+          {0, 1, 1}}
+Output:
+0
+*/
+
+class Matrix
 {
-    //Function to return list of integers visited in snake pattern in matrix.
-class Solution {
    int columnWithMaxZeros (int arr[][] , int N)
    {
-       
        int maxZero = 0;
        int answer = -1;
        for (int i=0; i<N; i++) {
