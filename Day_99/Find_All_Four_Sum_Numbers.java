d401195b5a95b46136fbfe910e751b07facd5d0d
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2024-02-14 12:57:30 +0530

    Add files via upload

---
+++ Day_99/Find_All_Four_Sum_Numbers.java
@@ -0,0 +1,73 @@
+/*
+Given an array A of integers and another number K. Find all the unique quadruple from the given array that sums up to K.
+
+Also note that all the quadruples which you return should be internally sorted, ie for any quadruple [q1, q2, q3, q4] the following should follow: q1 <= q2 <= q3 <= q4.
+
+Example 1:
+
+Input:
+N = 5, K = 3
+A[] = {0,0,2,1,1}
+Output: 0 0 1 2 
+Explanation: Sum of 0, 0, 1, 2 is equal
+to K.
+Example 2:
+
+Input:
+N = 7, K = 23
+A[] = {10,2,3,4,5,7,8}
+Output: 2 3 8 10 
+        2 4 7 10 
+        3 5 7 8 
+Explanation: Sum of 2, 3, 8, 10 = 23,
+sum of 2, 4, 7, 10 = 23 and sum of 3,
+5, 7, 8 = 23.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function fourSum() which takes the array arr[] and the integer k as its input and returns an array containing all the quadruples in a lexicographical manner. In the output each quadruple is separate by $. The printing is done by the driver's code.
+
+Expected Time Complexity: O(N3).
+Expected Auxiliary Space: O(N2).
+*/
+
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> fourSum(int[] arr, int target) {
+        int n = arr.length;
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && arr[j] == arr[j - 1]) continue;
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    long sum = arr[i] + arr[j] + arr[k] + arr[l];
+
+                    if (sum == target) {
+                        ArrayList<Integer> temp = new ArrayList<>();
+                        temp.add(arr[i]);
+                        temp.add(arr[j]);
+                        temp.add(arr[k]);
+                        temp.add(arr[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+
+                        while (k < l && arr[k] == arr[k - 1]) k++;
+                        while (k < l && arr[l] == arr[l + 1]) l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
